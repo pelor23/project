8d7290ecc1fe15bfd423a51a0b9a59ba9c4d13f0
Mateusz Szymański <mateusz_szymanski@interia.pl>
2016-03-15 01:26:26 +0100

    Adding a welcome text and boot the main loop by pressing the enter key.

--- game.py
+++ game.py
@@ -1,18 +1,18 @@
 # Here we import method exit() from module called sys
 from sys import exit
 
-# Here we import two things: firstly the library called pygame
-# Secondly from pygame.locals we import the entire contents
+# Here we import two things: firstly the library called pygame;
+# Secondly from pygame.locals we import the entire contents.
 import pygame
 from pygame.locals import *
 
-# Here we set window size
+# Here we set window size.
 SCREEN_SIZE = (800, 600)
 
 
-# Here is our main class
+# Here is our main class.
 class SpaceInvadersGame(object):
-    # Here is the method responsible for initializing and setting the pygame library and display windows
+    # Here is the method responsible for initializing and setting the pygame library and display windows.
     def __init__(self):
         pygame.init()
         flag = DOUBLEBUF
@@ -20,14 +20,42 @@ class SpaceInvadersGame(object):
         self.gamestate = 1
         self.loop()
 
-    # Here is the method, which is responsible for the termination of the game and exit to the main system
+        # Below we add welcome text and support the enter key.
+        ## This line of code clears the screen and sets the background color to black.
+        self.surface.fill((0, 0, 0))
+
+
+        ## Here we set the font.
+        ## We call it with the value None (at this point pygame load the default font used in pygame)
+        ## and set its size to 15.
+        myfont = pygame.font.Font(None, 15)
+        ## Font rendering with the inscription.
+        ## We set two additional parameters - apart from the content, add information, that font is to be smoothed,
+        ## and what is its color.
+        label = myfont.render("Press ENTER to start the game", 1, (255, 255, 0))
+        ## We add the label to display.
+        self.surface.blit(label, (100, 100))
+        ## We send here a signal to update the displayed image.
+        pygame.display.flip()
+        ## Additionally, we set the loop to listen pressing the enter button
+        ## and start the game and the handle shutdown the window and escape key.
+        while True:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN and event.key == K_RETURN:
+                    self.gamestate = 1
+                    self.loop()
+                if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE)):
+                    exit()
+
+    # Here is the method, which is responsible for the termination of the game and exit to the main system.
     def game_exit(self):
-        """ funkcja przerywa dzialanie gry i wychodzi do systemu"""
+        """ This function interrupts the action of the game and exit to the system"""
         exit()
 
-    # This is the main loop of the game, which supports the shutdown event and escape key.Here also will be located service of our game and events
+    # This is the main loop of the game, which supports the shutdown event and escape key.
+    # Here also will be located service of our game and events
     def loop(self):
-        """ glowna petla gry """
+        """ Main loop of the game """
         while self.gamestate == 1:
             for event in pygame.event.get():
                 if (event.type == QUIT or
